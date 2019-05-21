36b30d36c775e386da104350c5a4a8a89d00c267
Manik <dmanik@seznam.cz>
2019-05-21 19:04:40 +0200

    First commit

---
+++ data.php
@@ -0,0 +1,225 @@
+<?php
+$makers = array(
+	array(
+		'name' => 'Lujza Kotryová',
+	),
+	array(
+		'name' => 'Zbyněk "Kilien" Štajer',
+	),
+	array(
+		'name' => 'Michal "Maník" Havelka',
+	),
+	array(
+		'name' => 'Petr "Čiki" Kuběnský',
+	),
+);
+
+$games = array(
+	array(
+		'title' => 'L-World',
+		'image' => 'L-world.jpg',
+		'subtitle' => 'Partnership, friendship and parenthood at stake',
+		'annotation' => '
+ L-world is a game about partnership, friendship and parenthood. It takes place on private party of few friends. The host of the party are Sabine, who is very active in the LGBT movement, her wife Veronica and her daughter Denise.
+There are pre-written characters and given setting in the game, but the gameplay supports freedom of players. The game is less controlled by organizer, there are some organizer’s inputs during the game (NPC on phone). Secrets are important for the game.
+Most of the characters in the game live in a homosexual partnership. Nevertheless, the game does not deal with homosexual themes specifically. It focuses more on what people, and women especially, look for in their relationships, either in partnership or in family.
+The game is not inspired by so-called TV series.',
+		'short_annotation' => 'Sabine, very active in the LGBT movement, her wife Veronica, and her daughter Denise are looking for what is important to them in relationships, either in partnership or in family.',
+		'duration' => '4',
+		'players' => array(
+			'total' => 6,
+			'male' => 1,
+			'female' => 5,
+			'both' => 0
+		),
+		'authors' => 'Lucie Chlumská',
+		'year' => 2013,
+		'rating' => 8.5,
+		'dblink' => 'http://www.larpovadatabaze.cz/larp/l-world/265',
+	),
+	array(
+		'title' => 'Moon',
+		'image' => 'Moon.jpg',
+		'subtitle' => 'Freedom in times of western-style future',
+		'annotation' => '
+One day the world became too small for all of us. Then we started to settle other planets. Terraformation begun. Things changed. Lot of us became adventures, seeking freedom and independence. But with great power comes great responsibility… None of us had an idea of what the “Alliance” would be capable of... “Take my home, take my land, take me where I cannot stand. I don´t care, I´m still free, you can´t take the sky from me…” (Words from the Firefly theme)',
+		'short_annotation' => '“Take my home, take my land, 
+take me where I cannot stand. 
+I don´t care, I´m still free,
+ you can´t take the sky from me…”',
+		'duration' => '5',
+		'players' => array(
+			'total' => 10,
+			'male' => 6,
+			'female' => 3,
+			'both' => 1
+		),
+		'authors' => 'Filip Kábrt, Jindřich Mašek, Petr Platil, Roman Čech, Martin Buchtík',
+		'year' => 2011,
+		'rating' => 8.6,
+		'dblink' => 'http://www.larpovadatabaze.cz/larp/moon/20',
+	),
+	array(
+		'title' => 'Telenovela',
+		'image' => 'Telenovela.jpg',
+		'subtitle' => 'Love. Hate. Happiness. Fear. Friendship. Envy. And Love.',
+		'annotation' => '
+Love. Hate. Happiness. Fear. Friendship. Envy. And Love. Telenovela is a parody of all endless TV series full of earnest and genuine emotions. Families Lanz and Pereira have been torn apart by ancient wrongs and carefully hidden secrets. However, love will conquer all! The game takes place in present-day Venezuela or in a cheap film studio; depending on how much will the players in their roles of bad actors immerse into their parts. The larp is a tightly-scripted romantic crazy comedy divided into Scenes. It is an entertaining farce that contains all the clichés you have ever seen in bad TV series. "If you want a character that doesn\'t have a love plot in their story, you\'re at the wrong larp."
+',
+		'short_annotation' => 'A parody of all endless TV series full of earnest and genuine emotions. Families Lanz and Pereira have been torn apart by ancient wrongs and carefully hidden secrets. However, love will conquer all! ',
+		'duration' => '4',
+		'players' => array(
+			'total' => 14,
+			'male' => 7,
+			'female' => 6,
+			'both' => 1
+		),
+		'authors' => 'Kamil Buchtík, Radek Morávek',
+		'year' => 2013,
+		'rating' => 8.0,
+		'dblink' => 'http://www.larpovadatabaze.cz/larp/telenovela/79',
+	),
+	array(
+		'title' => 'Don Juan',
+		'image' => 'Don Juan.jpg',
+		'subtitle' => 'The night before marriage',
+		'annotation' => '
+The game takes place somewhere in the foothills of Scottish highlands in a dark cottage, on an evening before a wedding. It is a perfect opportunity for five childhood friends to meet again. One of them is getting married on the next day. In a traditional ritual of plucking feathers and girly chatting during the game, it is important to discuss all the life experience, joys and sorrow. At the end of this night, a young girl becomes an adult woman prepared for marriage. What will young Agnes have to leave behind?
+The game is divided into five acts, with almost theatrical parts. It is directed by the gamemaster playing an NPC interacting with the players. The storyline is not the main point; the game is based mainly on the atmosphere of girly chitchat and emotions around marriage. The atmosphere has a lot to do with the Theater of the Absurd.
+One of the aims of the game is to make people think about the way they choose their partners and what they expect of them.
+“Oh, you really do live in your little Neverland, don’t you? The world is cruel, baby doll! But you will get to learn that soon enough, no worries. And you will see what’s beyond the curtain!”
+',
+		'short_annotation' => '“Oh, you really do live in your little Neverland, don’t you? The world is cruel, baby doll! But you will get to learn that soon enough, no worries. And you will see what’s beyond the curtain!”',
+		'duration' => '4',
+		'players' => array(
+			'total' => 1,
+			'male' => 4,
+			'female' => 1,
+			'both' => 0
+		),
+		'authors' => 'Ondřej “Zak” Staněk (stanek.ondra@gmail.com), Tereza Králová',
+		'year' => 2013,
+		'rating' => 8.2,
+		'dblink' => '?????',
+	),
+	array(
+		'title' => 'Carnival Life',
+		'image' => 'Carnival Life.jpg',
+		'subtitle' => '1930s French avant-garde',
+		'annotation' => '',
+		'short_annotation' => 'The story of family secrets and repressed desires updates the timeless themes of love and death in the Paris of the 1930s. Out of the dust of the exhausted Dadaism, a new artistic movement has risen: surrealism.',
+		'duration' => '3.5-5',
+		'players' => array(
+			'total' => 1,
+			'male' => 4,
+			'female' => 1,
+			'both' => 0
+		),
+		'authors' => 'Petr “Čiki” Kuběnský',
+		'year' => 2010,
+		'rating' => 8.2,
+		'dblink' => '?????',
+	),
+	array(
+		'title' => 'Two Worlds',
+		'image' => 'Two Worlds.jpg',
+		'subtitle' => 'Teenage life torn apart',
+		'annotation' => '
+The game Two Worlds works with a strong story of a girl whose world is starting to crack slowly, crumble down and fall in between her fingers. People around her are unable to help her and they don\'t understand her problems. Whether it is her mother, her therapist, her partner or friends - everyone is doing what they can and it\'s still not enough. Her life gets divided into two worlds and she is gradually losing the grip on either of them. The game works with scenes from her life, maps her most important decisions, encounters and conversation with her loved ones. The atmosphere is partially influenced by lights and shadows that serve to induce various moods in the players and pull them deeper into the story. The course and conclusion of the game are relatively tightly scripted. Players have no power to influence what happens in the game. What is important is HOW it\'s going to happen. This impossibility to influence the course of game events and playing with interpretations of the individual scenes should be the source of deep experience players get from this game. Although the game begins cheerfully, its course is gloomy, sad, serious and sometimes even a bit surreal. The goal is to open the topic of coping with problems one is not ready for in adolescence. “Kathie, is that you?”
+		',
+		'short_annotation' => '“Kathie, is that you?”
+Her mother, her therapist, her partner or friends - everyone is doing what they can and it\'s still not enough. Her life gets divided into two worlds and she is gradually losing the grip on either of them.',
+		'duration' => '3-5',
+		'players' => array(
+			'total' => 1,
+			'male' => 2,
+			'female' => 3,
+			'both' => 1
+		),
+		'authors' => '',
+		'year' => 2009,
+		'rating' => 8.2,
+		'dblink' => '?????',
+	),
+	array(
+		'title' => 'Pika-don',
+		'image' => 'Pika-don.jpg',
+		'subtitle' => 'Top secret atomic meeting',
+		'annotation' => '
+August 1945. The war in Europe ended three months ago, but ferocious battles continue to rage in the Pacific. A plane with 6 men lands at the American Johnston Island Air Force Base. These men represent several nations; they are ambassadors, secret service agents but also clergymen. This top secret meeting has one item on the agenda – a new weapon, never used before – the atomic bomb. Before they use it for the first time, Americans want majority of the representatives of the countries involved in the ending war to give their consent. However, each of the participants has their own goals and the only question is how far they are willing to go to achieve them. Pika-Don is a diplomatic larp about decision-making, asserting one’s interests, on responsibility and mainly on the meaning of ethics and morality. Character development does not rely on players themselves, but on the decisions they make in the game. The larp demonstrates how varying information on which we base both small and big decisions as well as our opinions on crucial events (e.g. dropping the atomic bomb) impact how we take perspective.',
+		'short_annotation' => 'August 1945. A plane with 6 men lands at the American Johnston Island Air Force Base. The top secret meeting has one item on the agenda – a new weapon, never used before – the atomic bomb. ',
+		'duration' => '3',
+		'players' => array(
+			'total' => 1,
+			'male' => 0,
+			'female' => 0,
+			'both' => 6
+		),
+		'authors' => '',
+		'year' => 2012,
+		'rating' => 8.2,
+		'dblink' => '?????',
+	),
+	array(
+		'title' => 'Operating System',
+		'image' => 'Operating System.jpg',
+		'subtitle' => 'Family life in times of AI',
+		'annotation' => '
+The game is set in the near future. The players assume roles of a married couple, Patrick and Erica, their autistic son Mathias, and Erica\'s friend Claudia. The players of Mathias and Claudia also act as the voices of the operating system. The game is divided into scenes with brief descriptions, which give enough space for improvisation. The game story is determined from the start, but the players can influence the exact ways it is enacted, as well as its end. The goal of the larp is to make the players think about the ways the present and future technology, social networks, and artificial intelligence can influence interpersonal relationships.
+',
+		'short_annotation' => 'Discovering the ways the present and future technology, social networks, and artificial intelligence can influence interpersonal relationships.',
+		'duration' => '3.5',
+		'players' => array(
+			'total' => 1,
+			'male' => 2,
+			'female' => 2,
+			'both' => 0
+		),
+		'authors' => '',
+		'year' => 2014,
+		'rating' => 8.2,
+		'dblink' => '?????',
+	),
+	array(
+		'title' => 'The Ascent of Ouranos',
+		'image' => 'Ouranos.jpg',
+		'subtitle' => 'The soul\'s journey',
+		'annotation' => '',
+		'short_annotation' => 'The voyage of five souls through time, through key events of human lives, from completely intimate moments to great, historical ones. What impression the souls and their thoughts will make in completely different scenes?',
+		'duration' => '5',
+		'players' => array(
+			'total' => 1,
+			'male' => 3,
+			'female' => 2,
+			'both' => 0
+		),
+		'authors' => '',
+		'year' => 2013,
+		'rating' => 8.2,
+		'dblink' => '?????',
+	),
+	array(
+		'title' => 'Samael',
+		'image' => 'Samael.jpg',
+		'subtitle' => 'Fake smiles hide real tears',
+		'annotation' => '',
+		'short_annotation' => 'At the end of a high-school reunion, in the middle of the 80s, four former classmates are left in one bar at the frontier of bad dreams and even worse reality. In a whirlwind dance, they\'re falling deeper and deeper into their common dark past, icy fingers of their personal weaknesses hysterically tickling their nerves.',
+		'duration' => '3-4',
+		'players' => array(
+			'total' => 1,
+			'male' => 2,
+			'female' => 2,
+			'both' => 0
+		),
+		'authors' => '',
+		'year' => 2013,
+		'rating' => 8.2,
+		'dblink' => '?????',
+	),
+);
+
+
+
+function nl2p($string, $line_breaks = true, $xml = true) {
+	return preg_replace('/\n(\s*\n)*/', '</p><p>', $string);
+}
---
+++ index.php
@@ -0,0 +1,367 @@
+<?php
+	require "data.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta charset="utf-8">
+	
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+	<title>Check Larps</title>
+	<meta property="og:title" content="Check Larps">
+
+	<meta name="description" content="">
+	<meta property="og:description" content="">
+
+	<meta property="og:type" content="website">
+	<meta property="og:image" content="http://checklarps.com/img/og_image.jpg">
+	<meta property="og:title" content="Check larps">
+	<meta property="og:description" content="Czech larp collection book. 10 take & play larps, you may taste on your own.">
+	<meta property="og:locale" content="en_US">
+
+	<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<link rel="stylesheet" type="text/css" href="css/style.min.css?v=6">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/custom.css?v=0">
+	<script type="text/javascript" src="js/script.min.js"></script>
+
+	<link href="https://fonts.googleapis.com/css?family=Poppins:200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Zilla+Slab:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+	<!-- fancybox -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
+	<script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
+
+
+</head>
+<body>
+
+<div class="book-bg"></div>
+<div class="container container-sm fist-slide text-center">
+	<h1>
+		<div class="main-header">Check Larps</div>
+		<div class="mt-4 mt-lg-0">
+			<small class=main-subheader>10 take &amp; play larps from Czechia in one book</small>
+		</div>
+	</h1>
+
+	<div class="row">
+		<div class="col-sm-8 col-sm-offset-2">
+			<div class="row numbers">
+				<div class="col-sm-4 text-center">
+					<div class="big">10</div>
+					<p>take &amp; play games</p>
+				</div>
+				<div class="col-sm-4 text-center">
+					<div class="big">43</div>
+					<p>hours of fun</p>
+				</div>
+				<div class="col-sm-4 text-center">
+					<div class="big">200+</div>
+					<p>game runs</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+
+
+	<div class="row main-cta" id="insideCta">
+		<a href="#insideCta" class="cta cta-look" onclick="$('.inside-thumb')[0].click()">Look inside</a>
+	</div>
+</div>
+
+
+
+<div class="container container-sm text-center" id="inside">
+
+	<!-- <h2>Inside the book</h2> -->
+
+	<div class="inside-wrap">
+		<a class="inside-thumb" data-fancybox="gallery" href="img/inside/inside 01.png"><img src="img/inside/inside 01.png"></a>
+		<a class="inside-thumb" data-fancybox="gallery" href="img/inside/inside 02.png"><img src="img/inside/inside 02.png"></a>
+		<a class="inside-thumb" data-fancybox="gallery" href="img/inside/inside 03.png"><img src="img/inside/inside 03.png"></a>
+		<a class="inside-thumb" data-fancybox="gallery" href="img/inside/inside 04.png"><img src="img/inside/inside 04.png"></a>
+		<a class="inside-thumb" data-fancybox="gallery" href="img/inside/inside 05.png"><img src="img/inside/inside 05.png"></a>
+		<a class="inside-thumb" data-fancybox="gallery" href="img/inside/inside 06.png"><img src="img/inside/inside 06.png"></a>
+		<a class="inside-thumb" data-fancybox="gallery" href="img/inside/inside 07.png"><img src="img/inside/inside 07.png"></a>
+	</div>
+
+
+	<a href="#" class="cta cta-buy">Buy the book <span>$19.90</span></a>
+</div>
+
+
+<div class="container margin-top-10rem text-center" id="larps">
+	<h2>
+		larps
+	</h2>
+
+	<div class="row">
+		<?php foreach ($games as $key => $game) { ?>
+
+			<div class="col-xs-12 col-sm-10 col-sm-offset-1 <?php if ($key >= 3) echo 'area-hidden' ?>">
+					
+					<div class="row text-left">
+						<div class="col-xs-4">
+							<img class="img-responsive" src="img/larps/<?= $game['image'] ?>">
+						</div>
+						<div class="col-xs-8 col-lg-6">
+							<h1>
+								<?php echo $game['title']; ?>
+								<br>
+								<small><?php echo $game['subtitle']; ?></small>
+							</h1>
+							<h4><?= $game['players']['total'] ?> players, <?php echo $game['duration']; ?> hours
+							</h4>
+							
+							<p>
+								<?= $game['short_annotation'] ?>
+							</p>
+							<a class="cta cta-seeMore" data-toggle="modal" data-target=".game<?php echo $key; ?>" href="">See more</a>
+						</div>
+					</div>
+
+			</div>
+
+			<div class="modal fade game<?php echo $key; ?>" tabindex="-1" role="dialog" aria-labelledby="">
+				<div class="modal-dialog modal-lg">
+					<div class="modal-content">
+						<div class="modal-body">
+							<div class="row text-left">
+
+								<div class="col-xs-9 col-xs-offset-3">
+									<h1>
+										<?php echo $game['title']; ?>
+										<br>
+										<small><?php echo $game['subtitle']; ?></small>
+									</h1>
+								</div>
+								<div class="col-xs-3">
+									<img class="img-responsive" src="img/larps/<?= $game['image'] ?>">
+								</div>
+								<div class="col-xs-9">
+									<hr>
+									<p>
+										<?php echo nl2p($game['annotation']); ?>
+									</p>
+									<hr class="gameModal_line">
+									<div class="col-xs-4">
+										<div class="row text-center">
+											<div class="">
+												<img src="img/players.svg" class="gameModal__icon" alt="">			
+											</div>
+											<div class="">
+												<?php
+												$gamePlayers = '';
+												if (!empty($game['players']['male'])) {
+													$gamePlayers = $game['players']['male'] .' male';
+												}
+												if (!empty($game['players']['female'])) {
+													if (!empty($gamePlayers)) $gamePlayers .= ', ';
+													$gamePlayers .= $game['players']['female'] .' female';
+												}
+												if (!empty($game['players']['both'])) {
+													if (!empty($gamePlayers)) $gamePlayers .= ', ';
+													$gamePlayers .= $game['players']['both'] .' both';
+												}
+												echo $gamePlayers;
+												?>
+											</div>
+										</div>
+										
+									</div>
+									<div class="col-xs-4">
+										<div class="row text-center">
+											<div class="">
+												<img src="img/time.svg" class="gameModal__icon" alt="">			
+											</div>
+											<div class="">
+												<?php echo $game['duration']; ?> hours
+											</div>
+									</div>
+									</div>
+									<div class="col-xs-4">
+										<div class="row text-center">
+											<div class="">
+												<img src="img/authors.svg" class="gameModal__icon" alt="">			
+											</div>
+											<div class="">
+													<?php echo $game['authors']; ?>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="row text-center">
+								<a href="/materials" class="cta cta-materials">Download additional materials</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+
+		<?php } ?>
+	</div>
+
+
+	<div class="text-center show-more margin-top-3rem">
+		<a href="#larps" class="cta cta-look" onclick="document.querySelector('#larps').classList.add('expanded'); return false;">
+			Show them all
+		</a>
+	</div>
+
+</div>
+
+
+<div class="container margin-top-10rem text-center">
+	<h2>
+		Their thoughts
+	</h2>
+
+	<div class="row">
+		<div class="col-xs-12 col-md-8 col-md-offset-2">
+			<div class="testimonial">
+				<div class="testimonial-content">Top 11 take &amp; play chamber larps from Czech in one collection. You want that!</div>
+				<div class="testimonial-author">Claus Raasted</div>
+			</div>
+			<div class="testimonial">
+				<div class="testimonial-content">Top 11 take &amp; play chamber larps from Czech in one collection. You want that!</div>
+				<div class="testimonial-author">Claus Raasted</div>
+			</div>
+			<div class="testimonial">
+				<div class="testimonial-content">Top 11 take &amp; play chamber larps from Czech in one collection. You want that!</div>
+				<div class="testimonial-author">Claus Raasted</div>
+			</div>
+		</div>
+	</div>
+
+	<a href="#" class="">Buy the book <span>$19.90</span></a>
+</div>
+
+
+
+<div class="container margin-top-10rem " id="more">
+	<h2 class="text-center">
+		More about the book
+	</h2>
+
+	<div class="row">
+		<div class="col-xs-12 col-md-8 col-md-offset-2">
+
+			<div class="testimonial">
+				<div class="testimonial-content">“In the Czech republic there are more than 300 chamber larps designed and played repetitively. We had chosen 10 great ones of them and put them into one book so you can taste them on your own.</div>
+				<div class="testimonial-author">Lujza Kotryová, editor</div>
+			</div>
+			<p>
+				The book contains 10 full larpscripts including design concepts, tutorials, tips and tricks for facilitations and reflections. We had chosen larps that were played many times and they are proven by players. You don't find any raw ideas there that weren't tested repeatedly. Our aim is to offer you quality as well as diversity of scenarios typical for Czech larp scene.
+			</p>
+			<p>
+				You can find there detailed guidelines for running the larp by yourself. Additional materials, such as full texts for players, playlists, in-game texts and picture materials are available on the web for download after buying the book.
+			</p>
+
+<!-- 			<div class="text-center show-more margin-top-3rem">
+				<a href="#more" onclick="document.querySelector('#more').classList.add('expanded'); return false;">
+					Read on
+				</a>
+			</div>
+
+			<div class="area-hidden"> -->
+
+				<p>We, editors of this book, are larp enthusiasts, authors, larp theoreticians and members of Czech, and Europe, larp community. In 2015 we have decided to put together a collection of larps that represents our “Czech cousine”. From that time our firstly small project got fat and here we are translating 10 very well developed and complex full larps into english for international public.</p>
+
+				<p>Our hope is that this book helps not only preserve some of the good designs made so they won't disappear, but we also hope that that they can be interesting for you to study in terms of game design and that they will be played behind our borders. </p>
+
+				<p>We firmly believe that a style of larps representing the Czech way is unique in its forms and can be inspiring for other authors and organizers. So called chamber larps (in english sometimes called blackbox larps, theatre larps or parlor style larps) had gone through big development during 2007-2008 to repetitive take and play format with robust content. Around 2010 the style we call “czechoslovakian jeep-form” came across using some of the nordic jeep-form mechanics combining them with “classical” chamber larps. </p>
+
+				<p>Examples of both (classical Czech chamber larps and czechoslovakian jeep-form) awaits you in this book.</p>
+			<!-- </div> -->
+
+		</div>
+	</div>
+
+
+	<h2 class="text-center editors-heading">Meet the editors</h2>
+
+	<div class="row editors-row">
+		<div class="col-xs-12 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-0">
+			<div class="row">
+				<div class="col-xs-3 col-sm-2 col-sm-offset-1 col-md-3 col-md-offset-0">
+					<img src="img/editors/manik.jpg" class="img-responsive img-circle">
+				</div>
+				<div class="col-xs-9">
+					<h3 class="editor-name">Michal Havelka</h3>
+
+					<p>Michal plays larps because of amazing community around the fenomen. He cofounded larp database portal, organized a lot of festivals and was tutor on larp design course Škool. He is (co)author of nine larps and around thirty articles on larp portals. He loves good beer and tries to explore non-traditional ways of life.</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-xs-12 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-0">
+			<div class="row">
+				<div class="col-xs-3 col-sm-2 col-sm-offset-1 col-md-3 col-md-offset-0">
+					<img src="img/editors/lujza.jpg" class="img-responsive img-circle">
+				</div>
+				<div class="col-xs-9">
+					<h3 class="editor-name">Lujza Kotryová</h3>
+
+					<p>Lujza is author of several larps made in Slovakia and the Czech republic. She loves to cut edges of forms, topics and expectations in larp design. She works for Court of Moravia as a gamificator, game designer and consultant bringing playfulness (and larps) into bussinesses. She opened Škool - larp design school for non-larpers. You can read more about her work at her portfolio.</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-xs-12 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-0">
+			<div class="row">
+				<div class="col-xs-3 col-sm-2 col-sm-offset-1 col-md-3 col-md-offset-0">
+					<img src="img/editors/ciki.jpg" class="img-responsive img-circle">
+				</div>
+				<div class="col-xs-9">
+					<h3 class="editor-name">Petr Kuběnský</h3>
+
+					<p>Petr is writer, author of several larps and larp theoretician. He is dedicated to czech larp history and collects all stories of larp fathers to his new book. Also, he is standing behind the larp database portal. In his professional life he fall in love with experimental poetry and wrote a book about it.</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-xs-12 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-0">
+			<div class="row">
+				<div class="col-xs-3 col-sm-2 col-sm-offset-1 col-md-3 col-md-offset-0">
+					<img src="img/editors/hanka.jpg" class="img-responsive img-circle">
+				</div>
+				<div class="col-xs-9">
+					<h3 class="editor-name">Hana Kovářová</h3>
+
+					<p>Hana is absolvent of larp design course Škool which started her passion for this type of games both as player and author. She is interested in all kinds of games and fun and exploring their possibilities. She also studied a psychology.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</div>
+
+
+
+<div class="container margin-top-10rem text-center">
+	<a href="mailto:info@checklarps.com">info@checklarps.com</a>
+	|
+	With love from Brno...
+</div>
+
+
+<script>
+/*
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-6149741-16', 'auto');
+	ga('send', 'pageview');
+*/
+</script>
+
+</body>
+</html>
